7c86f4549531b2e6787379c2889a00d5a1a3dd60
Waken <votrevincentwaken@gmail.com>
2023-05-05 20:21:44 +0300

    Add project files.

---
+++ Homework16/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+namespace Homework16
+{
+    public class Program
+    {
+        static public void Main(string[] args)
+        {
+            Fraction[] fractionArr = new Fraction[3];
+            FillFractions(fractionArr);
+            ShowFractions(fractionArr);
+            SerializeFractions(fractionArr);
+            DeSerialize(fractionArr);
+        }
+        static public void FillFractions(Fraction[] fractionArr)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string[] input = Console.ReadLine().Split('/');
+                int numerator = int.Parse(input[0]);
+                int denominator = int.Parse(input[1]);
+                fractionArr[i] = (new Fraction(numerator, denominator));
+            }
+        }
+
+        static public void ShowFractions(Fraction[] fractionArr)
+        {
+            foreach (var item in fractionArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static public void SerializeFractions(Fraction[] fractionArr)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Fraction[]));
+            FileStream file = new FileStream("Fractions.xml", FileMode.Create);
+            serializer.Serialize(file, fractionArr);
+            file.Close();
+        }
+        static public void DeSerialize(Fraction[] fractionArr)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Fraction[]));
+            FileStream file = new FileStream("Fractions.xml", FileMode.Open);
+            Fraction[] desializer = ((Fraction[])serializer.Deserialize(file));
+
+            file.Close();
+        }
+        public class Fraction
+        {
+            public int numerator { get; set; }
+            public int denominator { get; set; }
+
+            public Fraction(int numerator, int denominator)
+            {
+                this.numerator = numerator;
+                this.denominator = denominator;
+            }
+            public Fraction()
+            {
+                this.numerator = 0;
+                this.denominator = 0;
+            }
+            public override string ToString()
+            {
+                return numerator + "/" + denominator;
+            }
+        }
+    }
+}
---
+++ SecondTask/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SecondTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+
+            Console.WriteLine("Enter Name:");
+            journal.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter publishing:");
+            journal.Publisher = Console.ReadLine();
+
+            Console.WriteLine("Enter Date (00.00.0000):");
+            journal.Date = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Page Count:");
+            journal.PageCount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(journal.ToString());
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Journal));
+            FileStream file = new FileStream("Journal.xml", FileMode.Create);
+            serializer.Serialize(file, journal);
+            file.Close();
+
+            XmlSerializer serializer2 = new XmlSerializer(typeof(Journal));
+            FileStream file2 = new FileStream("Journal.xml", FileMode.Open);
+            Journal desializer = ((Journal)serializer2.Deserialize(file2));
+            file2.Close();
+
+            Console.WriteLine(desializer);
+        }
+
+        public class Journal
+        {
+            public string Name { get; set; }
+            public string Publisher { get; set; }
+            public DateTime Date { get; set; }
+            public int PageCount { get; set; }
+
+            public override string ToString()
+            {
+                return
+                    $"Name: {Name}" +
+                    $"\nPublishing: {Publisher}" +
+                    $"\nDate: {Date:dd.MM.yyyy}" +
+                    $"\nPages: {PageCount}";
+            }
+
+        }
+    }
+}
